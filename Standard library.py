8a8ce6cb066f9f4c30eebce08b995654aa95dea0
Chris Yates <125972531+C-Yates@users.noreply.github.com>
2023-03-22 09:33:47 +0000

    Update Standard library.py
    Version 4

--- Standard library.py
+++ Standard library.py
@@ -10,17 +10,15 @@ alphabet = list(string.ascii_uppercase)
 
 # File location for macro generated drawings
 DRW_source = r"C:\LAC Conveyors\Standard Module CAD Library\Pallet\Roller Straight\Sections\Library Generation Related\Macro-Generated Drawings\\"
-# File location for BOMs (will become equal to generated eventually)
-XLS_source = r"C:\LAC Conveyors\Standard Module CAD Library\Pallet\Roller Straight\Sections\Library Generation Related\Master Drawings\\"
 # File location for issued drawings
 End_source = r"C:\LAC Conveyors\Standard Module CAD Library\Pallet\Roller Straight\Sections\Issued Standard Drawings\\"
 
 #------------------------------------------------------------ 
 # Primary function that runs all sub functions
-def CopyFiles(source_folder, destination_folder, bom_folder):
+def CopyFiles(source_folder, destination_folder):
     
     # List containing names of each BOM, also creates folder for each
-    BOM_List = FindExcelDocs(bom_folder)
+    BOM_List = FindExcelDocs(source_folder)
     
     # For each file in the generated folder obtain its location name
     for file in os.listdir(source_folder):
@@ -45,6 +43,7 @@ def CopyFiles(source_folder, destination_folder, bom_folder):
                 
     # Copy the top-level assembly drawing at the end
     CopyAssy(source_folder, destination_folder)
+    CopyBOM(source_folder, destination_folder)
     print("Operation completed")
 #------------------------------------------------------------ 
 def FindExcelDocs(source):
@@ -62,6 +61,12 @@ def FindExcelDocs(source):
             File_name.append(dirName)
             df = pd.read_excel(source + file, usecols="B")
             BOM = []
+            
+            # Removes indent spaces from each entry of the BOM
+            df = df.replace(
+                to_replace=' ', 
+                value='',
+                regex=True)
             
             # Add each part from a BOM to its own list then add this full list to the giant BOMs list
             for num in range(len(df)):
@@ -108,17 +113,17 @@ def CopyAssy(source_folder, destination_folder):
                 dest = destination_folder[:-2] + "\\" + File_name[i] + "\\"
                 destination = dest + file
                 # Attempt to create a valid assembly name
-                Assy = str(File_name[i]) + "-" + str(letter) + ".PDF"
+                Assy = str(File_name[i]) + "-" + str(letter) + ".pdf"
                 
                 # If the name we created matches a real assembly it will copy or update existing
                 if Assy == file:
                     try:
-                        shutil.copy(source, destination)
-                        print("Copied assembly", file)
-                    except:
                         os.chmod(destination, stat.S_IWRITE)
                         shutil.copy(source, destination)
-                        print("Updated assembly", file)
+                        print("Updated ", file)
+                    except:
+                        shutil.copy(source, destination)
+                        print("Copied ", file)
 #------------------------------------------------------------         
 def CreateSubFolders(location):
     
@@ -137,7 +142,7 @@ def CreateSubFolders(location):
 def sort(item, source, dest, file, option):
     
     # List of possible file types (PDF included twice due to some files using uppercase)
-    Types = [".SLDDRW", ".pdf", ".DXF", ".PDF"]
+    Types = [".pdf", ".dxf", ".DXF", ".PDF"]
     
     # Lists containing sub-folders and identifier types - must be corresponding
     for Type in Types:
@@ -165,19 +170,44 @@ def sort(item, source, dest, file, option):
                             print("Copied ", file)
                             
         # No need to cycle revisions so repeat the above without a rev at the end (e.g "-A")
-        else:
-            doc = str(item) + Type
-            if doc == file:
-                if item[-2:] == i[option]:
-                    destination = dest + Folders[option] + file
-                    try:
-                        os.chmod(destination, stat.S_IWRITE)
-                        shutil.copy(source, destination)
-                        print("Updated ", file)
-                    except:
-                        shutil.copy(source, destination)
-                        print("Copied ", file)
+            else:
+                doc = str(item) + Type
+                if doc == file:
+                    if item[-2:] == i[option]:
+                        destination = dest + Folders[option] + file
+                        try:
+                            os.chmod(destination, stat.S_IWRITE)
+                            shutil.copy(source, destination)
+                            print("Updated ", file)
+                        except:
+                            shutil.copy(source, destination)
+                            print("Copied ", file)
+#------------------------------------------------------------
+def CopyBOM(source_folder, destination_folder):
+        # Sets the index position in the list [File_name] for each entry (a.k.a BOM)
+    for File in File_name:
+        i = File_name.index(File)
+
+        # Cycle each file in the generated folder and set its current location & destination..
+        # Attempt to copy the BOM if the file for it existis as a .xls or .xlsx
+        for file in os.listdir(source_folder):            
+            source = source_folder + file
+            dest = destination_folder[:-2] + "\\" + File_name[i] + "\\"
+            destination = dest + file
+            # Attempt to create a valid BOM name
+            BOM_1 = str(File_name[i]) + ".xls"
+            BOM_2 = str(File_name[i]) + ".xlsx"
+
+            # If the name we created matches a real BOM it will copy or update existing
+            if BOM_1 == file or BOM_2 == file:
+                try:
+                    os.chmod(destination, stat.S_IWRITE)
+                    shutil.copy(source, destination)
+                    print("Updated ", file)
+                except:
+                    shutil.copy(source, destination)
+                    print("Copied ", file)  
 #------------------------------------------------------------
 
 # EXECUTE ORDER 66...
-CopyFiles(DRW_source, End_source, XLS_source)
+CopyFiles(DRW_source, End_source)
